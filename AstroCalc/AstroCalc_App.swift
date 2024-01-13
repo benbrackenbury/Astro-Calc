9e9b1a094a1136dcc9eea1024a5fe25c8963d33c
Ben Brackenbury <benbrack22@outlook.com>
2024-01-13 08:33:54 +0000

    cleanup

---
+++ AstroCalc/AstroCalc_App.swift
@@ -0,0 +1,18 @@
+//
+//  App.swift
+//  AstroCalc
+//
+//  Created by Ben Brackenbury on 13/01/2024.
+//  Copyright © 2024 Ben Brackenbury. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AstroCalc_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
